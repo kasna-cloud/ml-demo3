37d5ab686c492be45dd9e4b61111c27923d39e1d
Troy Bebee <troy.bebee@kasna.com.au>
2022-02-15 06:59:42 +1100

    Updates for GCP

--- app/push.sh
+++
@@ -1,5 +0,0 @@
-az acr login -n eliiza \
-&& docker push eliiza.azurecr.io/radio-monitor:latest-base \
-&& docker push eliiza.azurecr.io/radio-monitor:2.1.14-base \
-&& docker push eliiza.azurecr.io/radio-monitor:latest-deepspeech_0.9.3 \
-&& docker push eliiza.azurecr.io/radio-monitor:2.1.14-deepspeech_0.9.3
